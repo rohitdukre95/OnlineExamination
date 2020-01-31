910b6f6cf1d0d5f71a4c18567b12a608b5f4d59c
Siddhesh Lad <Siddhesh.Lad@atidan.com>
2020-01-31 12:20:19 +0530

    Postion View and controller

---
+++ OnlineExaminationPortal/Controllers/PositionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineExaminationPortal.Models;
+using OnlineExaminationPortal.Repository;
+
+namespace OnlineExaminationPortal.Controllers
+{
+    [AllowAnonymous]
+    public class PositionController : Controller
+    {
+        private readonly IRepository<Position> posRepository;
+
+        public PositionController(IRepository<Position> posRepository)
+        {
+            this.posRepository = posRepository;
+        }
+        public IActionResult Index()
+        {
+            var model = posRepository.GetAll().Where(pos => pos.IsActive == true);
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult DeletePosition(int id)
+        {
+            Position position = null;
+            if (id != 0)
+            {
+                position = posRepository.Get(id);
+            }
+
+            return View(position);
+        }
+
+        //[HttpPost]
+        //public IActionResult DeletePosition(int id)
+        //{
+        //    Position position = posRepository.Get(id);
+        //    if (position != null)
+        //    {
+        //        position.IsActive = false;
+        //        posRepository.Update(position);
+        //    }
+
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
