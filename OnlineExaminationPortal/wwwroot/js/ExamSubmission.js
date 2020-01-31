178908ef993caeebf0b079870cec6c1531764577
rohitdukre95 <rohit.dukre@atidan.com>
2020-01-31 19:08:24 +0530

    Commit getquestion detail on next button and back button

--- OnlineExaminationPortal/wwwroot/js/ExamSubmission.js
+++ OnlineExaminationPortal/wwwroot/js/ExamSubmission.js
@@ -1,24 +1,61 @@
 ﻿
 var examSubmissionService = new ExamSubmissionService();
 
-$(window).load(function () {
-    //examSubmissionService.GetQuestionsAlreadySavedData(CandidateQuestionData).then(function (response) {
-    //    sourceEditor.setValue(decode(data["source_code"]));
-    //    $selectLanguage.dropdown("set selected", data["language_id"]);
-    //    $compilerOptions.val(data["compiler_options"]);
-    //    $commandLineArguments.val(data["command_line_arguments"]);
-    //    stdinEditor.setValue(decode(data["stdin"]));
-    //    stdoutEditor.setValue(decode(data["stdout"]));
-    //    stderrEditor.setValue(decode(data["stderr"]));
-    //    compileOutputEditor.setValue(decode(data["compile_output"]));
-    //    sandboxMessageEditor.setValue(decode(data["message"]));
-    //    var time = (data.time === null ? "-" : data.time + "s");
-    //    var memory = (data.memory === null ? "-" : data.memory + "KB");
-    //    $statusLine.html(`${data.status.description}, ${time}, ${memory}`);
-    //    changeEditorLanguage();
+//$(window).load(function () {
+//    examSubmissionService.GetQuestionsAlreadySavedData(CandidateQuestionData).then(function (response) {
+//        sourceEditor.setValue(decode(data["source_code"]));
+//        $selectLanguage.dropdown("set selected", data["language_id"]);
+//        $compilerOptions.val(data["compiler_options"]);
+//        $commandLineArguments.val(data["command_line_arguments"]);
+//        stdinEditor.setValue(decode(data["stdin"]));
+//        stdoutEditor.setValue(decode(data["stdout"]));
+//        stderrEditor.setValue(decode(data["stderr"]));
+//        compileOutputEditor.setValue(decode(data["compile_output"]));
+//        sandboxMessageEditor.setValue(decode(data["message"]));
+//        var time = (data.time === null ? "-" : data.time + "s");
+//        var memory = (data.memory === null ? "-" : data.memory + "KB");
+//        $statusLine.html(`${data.status.description}, ${time}, ${memory}`);
+//        changeEditorLanguage();
 
+//    });
 //});
+
+
+$(window).on('load', function () {
+    //if ($('#SubmissionResult_ID').val() !== undefined && $('#SubmissionResult_ID').val() !== null) {
+    //    var SubmissionData = $('#SubmissionResult_ID').val();
+    //    var obj = JSON.parse(SubmissionData);
+    //    sourceEditor.setValue(obj.sourceCode);
+    //    //$(".view-lines").text(obj.sourceCode);
+    //    //sourceEditor.val(obj.sourceCode);
+    //    //        $selectLanguage.dropdown("set selected", data["language_id"]);
+    //    //$compilerOptions.val(obj.compilerOptions);
+    //    //$commandLineArguments.val(obj.commandLineArguments);
+    //    //stdinEditor.val(obj.standardInput);
+    //    //stdoutEditor.val(obj.standardOutput);
+    //    //stderrEditor.val(obj.standardError);
+    //    //compileOutputEditor.val(obj.compileTimeOutput);
+    //    //sandboxMessageEditor.val(obj.sandboxMessage);
+    //    //        var time = (data.time === null ? "-" : data.time + "s");
+    //    //        var memory = (data.memory === null ? "-" : data.memory + "KB");
+    //    //$statusLine.html(`${data.status.description}, ${time}, ${memory}`);
+    //    //        changeEditorLanguage();
+
+   // }
+
+    setTimeout(callonload, 1000);
+
+    //var name = '@Html.Raw(Model.SubmissonResult.SourceCode)';
+
 });
+
+function callonload() {
+    if ($('#SubmissionResult_ID').val() !== undefined && $('#SubmissionResult_ID').val() !== null) {
+        var SubmissionData = $('#SubmissionResult_ID').val();
+        var obj = JSON.parse(SubmissionData);
+        sourceEditor.setValue(obj.sourceCode);
+    }
+}
 
 function SubmitAnswer(nextPageNumber, positionId, questionNumber, candidateId) {
 
