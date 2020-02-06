ac02b8f7ac149ed5a5c2158c21a22953282548cb
rohitdukre95 <rohit.dukre@atidan.com>
2020-02-06 19:32:18 +0530

    Implemented Timer

--- OnlineExaminationPortal/wwwroot/js/ExamSubmission.js
+++ OnlineExaminationPortal/wwwroot/js/ExamSubmission.js
@@ -1,6 +1,40 @@
 ﻿
 var examSubmissionService = new ExamSubmissionService();
 
+//Starting of show Timer for exam 
+if (localStorage.getItem("total_seconds")) {
+    var total_seconds = localStorage.getItem("total_seconds");
+} else {
+    var total_seconds = 2 * 10;
+}
+var minutes = parseInt(total_seconds / 60);
+var seconds = parseInt(total_seconds % 60);
+function countDownTimer() {
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    } if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    document.getElementById("quiz-time-left").innerHTML = "Time Left :" + minutes + " minutes " + seconds + " seconds";
+    if (total_seconds <= 0) {
+        var backPageNumber = $('#back_page_number').val();
+        var positionId = $('#position_id').val();
+        var questionNumber = $('#question_number').val();
+        var candidateId = $('#candidate_id').val();
+        localStorage.clear();
+        setTimeout(SubmitAnswer(backPageNumber, positionId, questionNumber, candidateId, 'SubmitTest'), 1);      
+    } else {
+        total_seconds = total_seconds - 1;
+        minutes = parseInt(total_seconds / 60);
+        seconds = parseInt(total_seconds % 60);
+        localStorage.setItem("total_seconds", total_seconds);
+        setTimeout("countDownTimer()", 1000);
+    }
+}
+setTimeout("countDownTimer()", 1000);
+//End of Show Timer
+
 //$(window).load(function () {
 //    examSubmissionService.GetQuestionsAlreadySavedData(CandidateQuestionData).then(function (response) {
 //        sourceEditor.setValue(decode(data["source_code"]));
