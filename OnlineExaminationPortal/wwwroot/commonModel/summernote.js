9fa93c97c518b0263b7544e8ccbcf1d7be9c060d
rohitdukre95 <rohit.dukre@atidan.com>
2020-02-14 17:18:07 +0530

    added summernote in commonmodel.js

---
+++ OnlineExaminationPortal/wwwroot/commonModel/summernote.js
@@ -0,0 +1,19 @@
+﻿function getSummerNote(id) {
+    if (id !== "" && id !== undefined)
+    {
+        $('#' + id).summernote({
+            placeholder: 'Write question here...',
+            tabsize: 2,
+            height: 120,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+        });
+    }
+}
